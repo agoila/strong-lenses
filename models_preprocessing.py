1a4b55d4ceb6d0c30322944700c197bb3edd387b
Ankit Goila <agoila@umich.edu>
2017-09-25 10:56:05 -0400

    latest pull from branch 'daniel'

--- models_preprocessing.py
+++ models_preprocessing.py
@@ -16,7 +16,8 @@ def compiledConvnet(input_shape=(101, 101, 4)):
 
 def convnet(input_shape=(101, 101, 4)):
 	model = Sequential()
-	model.add(Conv2D(64, (3, 3), strides=(2,2), activation='softplus', input_shape=input_shape))
+	model.add(Conv2D(64, (3, 3), strides=(2,2), activation='softplus',
+					input_shape=input_shape))
 	model.add(Conv2D(32, (3, 3), strides=(2,2), activation='softplus'))
 	model.add(Conv2D(16, (3, 3), activation='softplus'))
 	model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
@@ -88,7 +89,8 @@ def compiledRegularizedConvnet(input_shape=(101, 101, 4)):
 reg = 0.3
 def regularizedConvnet(input_shape=(101, 101, 4)):
 	model = Sequential()
-	model.add(Conv2D(64, (3, 3), strides=(2,2), activation='softplus', input_shape=input_shape))
+	model.add(Conv2D(64, (3, 3), strides=(2,2), activation='softplus',
+					input_shape=input_shape))
 	model.add(Conv2D(32, (3, 3), strides=(2,2), activation='softplus'))
 	model.add(Conv2D(16, (3, 3), activation='softplus'))
 	model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
